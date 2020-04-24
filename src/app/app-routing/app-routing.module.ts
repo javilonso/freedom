62a23a6fb481b08d86920cbe0632c861c36854f5
javi <jmaker3d@gmail.com>
2020-04-24 20:53:14 +0200

    dfegrdede

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [{path:'home',component:HomeComponent},
                         {path: '**', redirectTo:'/home', pathMatch:'full'}];
  
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
